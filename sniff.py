a56a70c6aa76bf6b692eb99c59caf41558989591
oscariglesias21 <iglesiaso@uninorte.edu.co>
2024-03-31 22:32:29 -0500

    cambio linea 12

--- sniff.py
+++ sniff.py
@@ -9,7 +9,7 @@ UDP_PORT = 20000
 # Configuración del servidor web
 web_server_url = "http://52.201.18.119:80/updateFromSniffer"
 web_server_url1 = "http://54.211.70.225:80/updateFromSniffer"
-web_server_url2 = "http://44.198.179.134:80/updateFromSniffer"
+web_server_url2 = "http://3.82.103.211:80/updateFromSniffer"
 
 def extract_gps_info(data):
     # Simula la extracción de los datos GPS del payload
