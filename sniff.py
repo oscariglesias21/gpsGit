c5713270da477e91aa06b1ca524fe3f047d9c021
oscariglesias21 <iglesiaso@uninorte.edu.co>
2024-05-12 15:09:04 -0500

    prueba

--- sniff.py
+++ sniff.py
@@ -12,12 +12,11 @@ web_server_url1 = "http://54.211.70.225:80/FromSniffer"
 web_server_url2 = "http://3.82.103.211:80/FromSniffer"
 
 def extract_gps_info(data):
-    # Decodificar los datos y dividir por comas
     decoded_data = data.decode('utf-8','replace').strip().split(',')
     id_str, latitud_str, longitud_str, timestamp_str, *optional = decoded_data
-    id = str(id_str.split(":")[1])
-    latitud = float(latitud_str.split(":")[1])
-    longitud = float(longitud_str.split(":")[1])
+    id = id_str.split(":")[1].strip()
+    latitud = float(latitud_str.split(":")[1].strip())
+    longitud = float(longitud_str.split(":")[1].strip())
     timestamp_str = timestamp_str.replace("Timestamp:", "").strip()
     datetime_obj = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
     fecha = datetime_obj.strftime("%Y-%m-%d")
@@ -31,11 +30,11 @@ def extract_gps_info(data):
         "Time": hora
     }
 
-    # Verificar si hay información opcional de RPM
-    if optional:
-        rpm_str = optional[0]
-        rpm = int(rpm_str.split(":")[1])
-        gps_info["RPM"] = rpm
+    if optional and optional[0]:
+        rpm_str = optional[0].strip()
+        if ":" in rpm_str:  # Asegurarse de que el formato incluya los dos puntos
+            rpm = int(rpm_str.split(":")[1])
+            gps_info["RPM"] = rpm
 
     return gps_info
 
@@ -55,6 +54,5 @@ print(f"Escuchando en UDP {UDP_IP}:{UDP_PORT}")
 while True:
     data, addr = sock.recvfrom(1024) 
     print(f"Recibido mensaje: {data} de {addr}")
-    # Extraer y enviar los datos GPS
     gps_data = extract_gps_info(data)
     send_to_web_server(gps_data)
