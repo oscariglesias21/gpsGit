d8f5634896416b07a30a24bcbaeb568d098adeb8
oscariglesias21 <iglesiaso@uninorte.edu.co>
2024-05-12 12:32:43 -0500

    prueba sniffer

--- sniff.py
+++ sniff.py
@@ -1,6 +1,7 @@
 import socket
 import requests
 from datetime import datetime
+
 # Configuración del socket
 UDP_IP = "0.0.0.0"  # Escuchar en todas las interfaces
 UDP_PORT = 20000
@@ -11,18 +12,16 @@ web_server_url1 = "http://54.211.70.225:80/FromSniffer"
 web_server_url2 = "http://3.82.103.211:80/FromSniffer"
 
 def extract_gps_info(data):
-    # Simula la extracción de los datos GPS del payload
-    # Asegúrate de adaptar esta parte al formato específico de tus datos
-    decoded_data = data.decode('utf-8','replace').strip().split(',')    
+    # Decodificar los datos y dividir por comas
+    decoded_data = data.decode('utf-8','replace').strip().split(',')
     id_str, latitud_str, longitud_str, timestamp_str, *optional = decoded_data
     id = str(id_str.split(":")[1])
     latitud = float(latitud_str.split(":")[1])
     longitud = float(longitud_str.split(":")[1])
     timestamp_str = timestamp_str.replace("Timestamp:", "").strip()
     datetime_obj = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
-    fecha = datetime_obj.strftime("%Y-%m-%d") 
-    hora = datetime_obj.strftime("%H:%M:%S")  # Formato de hora a HH:MM:SS
-    rpm = int(rpm_str.split(":")[1])
+    fecha = datetime_obj.strftime("%Y-%m-%d")
+    hora = datetime_obj.strftime("%H:%M:%S")
 
     gps_info = {
         "Id": id,
@@ -32,6 +31,7 @@ def extract_gps_info(data):
         "Time": hora
     }
 
+    # Verificar si hay información opcional de RPM
     if optional:
         rpm_str = optional[0]
         rpm = int(rpm_str.split(":")[1])
@@ -47,9 +47,11 @@ def send_to_web_server(web_data):
             print(f"Datos enviados a {url}. Respuesta: {response.status_code}")
     except Exception as e:
         print(f"Error al enviar datos al servidor web: {e}")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 print(f"Escuchando en UDP {UDP_IP}:{UDP_PORT}")
+
 while True:
     data, addr = sock.recvfrom(1024) 
     print(f"Recibido mensaje: {data} de {addr}")
